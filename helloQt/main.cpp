3de33df87321614bd102e730417c9b150cc9f1d0
Sergio Ricardo Carlos <sergiorc@gmail.com>
2022-03-30 15:47:43 -0300

    Change made to include cooperation between QtCreator and Github.

--- helloQt/main.cpp
+++ helloQt/main.cpp
@@ -6,7 +6,9 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    qDebug() << "Hello World!";
+    qDebug() << "Hello World!\n";
+
+    qDebug() << "Now github and QtCreator are getting along with each other\n";
 
     return a.exec();
 }
